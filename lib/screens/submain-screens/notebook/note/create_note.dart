499cae995db451a0fd4b4caad0a5c72a47091601
vansh132 <shah.vansh132@gmail.com>
2023-01-20 23:30:49 +0530

    create note screen - added

--- lib/screens/submain-screens/notebook/note/create_note.dart
+++ lib/screens/submain-screens/notebook/note/create_note.dart
@@ -7,10 +7,77 @@ class CreateNoteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _nameController = TextEditingController();
+    final _descriptionController = TextEditingController();
+    final _tagController = TextEditingController();
+
+    void _addNote() {
+      print(_nameController.text);
+      print(_descriptionController.text);
+      print(_tagController.text);
+    }
+
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text("create note screen"),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              // colors: [Color(0xff8EC5FC), Color(0xffE0C3FC)], //final - 1
+              colors: [Color(0xffADD8FF), Color(0xffEBF5FF)], //final - 1
+              stops: [0.4, 0.7],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.only(
+            top: 10,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 550,
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                controller: _nameController,
+                onSubmitted: (_) => _addNote(),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+              width: 550,
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                maxLines: 4,
+                controller: _descriptionController,
+                onSubmitted: (_) => _addNote(),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              width: 550,
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Tag'),
+                controller: _tagController,
+                onSubmitted: (_) => _addNote(),
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            ElevatedButton(
+              onPressed: _addNote,
+              child: const Text("Add Note"),
+            )
+          ],
+        ),
       ),
     );
   }
