12ec61778d2ecbb2ccdf72261e99b0b9ed9bd0f3
vansh132 <shah.vansh132@gmail.com>
2023-01-21 19:14:23 +0530

    app bar - improvised

--- lib/screens/submain-screens/notebook/note/create_note.dart
+++ lib/screens/submain-screens/notebook/note/create_note.dart
@@ -67,7 +67,35 @@ class _CreateNoteScreenState extends State<CreateNoteScreen> {
     }
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Center(child: Text("Create Note")),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Row(
+                  children: [
+                    TextButton(
+                      child: const Text(
+                        "Log out",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        print("Log out ");
+                      },
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Icon(Icons.logout_rounded),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
